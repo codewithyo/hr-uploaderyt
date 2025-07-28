bbff897f4261a1432f46bb7c29e367aa5a513097
codewithyo <102235878+codewithyo@users.noreply.github.com>
2025-07-28 21:10:46 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,9 +7,9 @@ class Config:
 
     SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
 
-    API_ID = int(os.environ.get("API_ID"))
+    API_ID = int(os.environ.get("28583543"))
 
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("24fd6e53b6034ccbda0bfd62dfd47ad3")
 
     CLIENT_ID = os.environ.get("180064799903-ihosiokdglohin0giqr0pg872j5i76t7.apps.googleusercontent.com")
 
@@ -17,9 +17,9 @@ class Config:
 
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 
-    AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
+    AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "7990155194,7612404009")
 
-    AUTH_USERS = [BOT_OWNER, 374321319] + (
+    AUTH_USERS = [BOT_OWNER, 7990155194] + (
         [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
         if AUTH_USERS_TEXT
         else []
@@ -35,11 +35,11 @@ class Config:
 
     VIDEO_TITLE_PREFIX = os.environ.get("VIDEO_TITLE_PREFIX", "")
 
-    VIDEO_TITLE_SUFFIX = os.environ.get("VIDEO_TITLE_SUFFIX", "")
+    VIDEO_TITLE_SUFFIX = os.environ.get("VIDEO_TITLE_SUFFIX", "by HR")
 
     DEBUG = bool(os.environ.get("DEBUG"))
 
-    UPLOAD_MODE = os.environ.get("UPLOAD_MODE") or False
+    UPLOAD_MODE = os.environ.get("unlisted") or False
     if UPLOAD_MODE:
         if UPLOAD_MODE.lower() in ["private", "public", "unlisted"]:
             UPLOAD_MODE = UPLOAD_MODE.lower()
